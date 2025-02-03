c3fbf2c32971ffbaa64811f2e0dcac0f9551be59
zhaowenhui <1487793439@qq.com>
2025-02-03 21:23:30 +0800

    --

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from enum import Enum
-
-class test(Enum):
-    USERID = (1, 'userid', 'table')
-
-    def __init__(self, id, username, table):
-        self.id = id
-        self.username = username
-        self.table = table
-
-print(test.USERID)
-print(test.USERID.id)
+# test
